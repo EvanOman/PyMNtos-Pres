187cd9a7e51bbaca9a7902179c714c5ceb3baf2a
Evan Oman <evan058@gmail.com>
2016-02-16 23:06:33 -0600

    Cleaning ..... :rage1:

--- python/escapeImages.py
+++ python/escapeImages.py
@@ -1,15 +1,17 @@
 import numpy as np
 import math
-import scipy.ndimage
-import matplotlib.image
-from PIL import Image
+# from scipy import ndimage
+import matplotlib.pyplot as plt
+# from PIL import Image
 import time
-import pylab
+# import pylab
 
-def singPertParam(n1, d, beta, angle, n, m, itermax, xmin, xmax, ymin, ymax,filename,colorMap):
+
+def singPertParam(n1, d, beta, angle, n, m, itermax, xmin, xmax, ymin, ymax, filename, colorMap):
     """
-    Creates a 2 dimensional PARAMETER image of a singular perturbation of the complex quadratic map
-    z = z**n1 + c2 + (beta)/(z.conjugate()**d)
+    Creates a 2 dimensional PARAMETER image of a singular perturbation of the complex quadratic map:
+        z = z**n1 + c2 + (beta)/(z.conjugate()**d)
+
     :param n1: exponent of main phase term
     :param d: exponent of conjugate phase term
     :param beta: perturbation parameter
@@ -22,16 +24,17 @@ def singPertParam(n1, d, beta, angle, n, m, itermax, xmin, xmax, ymin, ymax,file
     :param ymin:
     :param ymax:
     :param filename:
-    :param colorMap:
-    :return:
+    :param colorMap: string name of Pillow colormap to use
+    :return: null, writes image to file
     """
 
     start = time.time()
 
-    #First we need to calculate the critical point that we are going to iterate on:
+    # First we need to calculate the critical point that we are going to iterate on:
     r = pow((d/n1)*abs(beta), 1.0/(n1+d))
 
-    #The set of critical points is a circle with the radius given by above, for now we will take the right-most real value on this circle:
+    # The set of critical points is a circle with the radius given by above, for now we will take the right-most real
+    # value on this circle:
     critPoint = r * complex(math.cos(angle), math.sin(angle))
 
 
@@ -41,8 +44,8 @@ def singPertParam(n1, d, beta, angle, n, m, itermax, xmin, xmax, ymin, ymax,file
     x = np.linspace(xmin, xmax, n)[ix]
     y = np.linspace(ymin, ymax, m)[iy]
 
-    c = x+complex(0,1)*y
-    del x, y # save a bit of memory, we only need z
+    c = x + complex(0, 1) * y
+    del x, y  # save a bit of memory, we only need z
 
     img = np.zeros(c.shape, dtype=int)
 
@@ -50,36 +53,38 @@ def singPertParam(n1, d, beta, angle, n, m, itermax, xmin, xmax, ymin, ymax,file
     iy.shape = n*m
     c.shape = n*m
 
-    #Sets all of the z_0 values
+    # Sets all of the z_0 values
     if critPoint == 0:
         z = critPoint**n1 + np.copy(c)
     else:
         z = critPoint**n1 + np.copy(c) + beta / (critPoint.conjugate()**d)
 
-
     for i in range(itermax):
-        if not len(z): break # all points have escaped
 
-        #multiply(z, z, z)
-        #add(z, c, z)
+        # If there aren't any values left, stop iterating
+        if not len(z):
+            break  # all points have escaped
 
-        z = pow(z, n1) + c + (beta)/(pow(z.conjugate(), d))
+        # Perform an iteration of our map
+        z = pow(z, n1) + c + beta / (pow(z.conjugate(), d))
 
-        # these are the points that have escaped
-        rem = abs(z)>2.0
+        # A logical vector specifying the points which have escaped
+        rem = abs(z) > 2.0
 
+        # Store the iteration, i, at which the rem points escaped
         img[ix[rem], iy[rem]] = i+1
 
+        # Now rem represents the points which have remained bounded
         rem = -rem
 
-        z = z[rem]
-        ix, iy = ix[rem], iy[rem]
-        c = c[rem]
+        # Filter out all escaped values
+        z, ix, iy, c = z[rem], ix[rem], iy[rem], c[rem]
+
     print('Time taken:', time.time() - start)
 
-    img[img==0] = itermax + 1
+    img[img == 0] = itermax + 1
 
-    image = imshow(img.T, origin='lower left')
+    image = plt.imshow(img.T, origin='lower left')
     if not colorMap == "":
         image.set_cmap(colorMap)
         image.write_png(filename+'.png', noscale=True)
@@ -90,15 +95,15 @@ def singPertParam(n1, d, beta, angle, n, m, itermax, xmin, xmax, ymin, ymax,file
 def singPertPhase(n1,d,beta,c2,n, m, itermax, xmin, xmax, ymin, ymax,filename,colorMap):
     start = time.time()
 
-    ix, iy = mgrid[0:n, 0:m]
+    ix, iy = np.mgrid[0:n, 0:m]
 
-    x = linspace(xmin, xmax, n)[ix]
-    y = linspace(ymin, ymax, m)[iy]
+    x = np.linspace(xmin, xmax, n)[ix]
+    y = np.linspace(ymin, ymax, m)[iy]
 
     z = x+complex(0,1)*y
     del x, y # save a bit of memory, we only need z
 
-    img = zeros(z.shape, dtype=int)
+    img = np.zeros(z.shape, dtype=int)
 
     ix.shape = n*m
     iy.shape = n*m
@@ -128,7 +133,7 @@ def singPertPhase(n1,d,beta,c2,n, m, itermax, xmin, xmax, ymin, ymax,filename,co
 
     img[img==0] = itermax + 1
 
-    image = imshow(img.T, origin='lower left')
+    image = plt.imshow(img.T, origin='lower left')
     if not colorMap == "":
         image.set_cmap(colorMap)
         image.write_png(filename+'.png', noscale=True)
